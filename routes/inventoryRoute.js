e16b638040a07537040967677b359c1b6adbf802
Nwachukwu Ogbonnaya Prince <princenwachukwu308@yahoo.com>
2024-10-07 02:44:23 +0100

    server error added

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -8,4 +8,11 @@ router.get("/type/:classificationId", invController.buildByClassificationId);
 //Route to build single car
 router.get("/detail/:invId", invController.buildByInvId);
 
+//Error handeling
+router.get("/trigger-error", (req, res, next) => {
+  const error = new Error("This is a Server Error.");
+  error.status = 500; // Set the error status code
+  next(error); // Pass the error to the next middleware
+});
+
 module.exports = router;
