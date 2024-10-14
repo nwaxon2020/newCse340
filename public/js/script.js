c1e84ed33154ef52408fd271eb98a2cc17b623fa
Nwachukwu Ogbonnaya Prince <princenwachukwu308@yahoo.com>
2024-10-14 13:19:33 +0100

    password visible

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+const pword = document.getElementById("register_password");
+const toogleEye = document.querySelector(".fa");
+
+toogleEye.addEventListener("click", () => {
+  if (toogleEye.classList.contains("fa-eye")) {
+    toogleEye.classList.remove("fa-eye");
+    toogleEye.classList.add("fa-eye-slash");
+
+    pword.setAttribute("type", "text");
+  } else {
+    toogleEye.classList.remove("fa-eye-slash");
+    toogleEye.classList.add("fa-eye");
+
+    pword.setAttribute("type", "password");
+  }
+});
